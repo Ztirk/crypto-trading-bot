c859015cc2404e391efa344ef1b910c99d8dc5d2
Ztirk <krit15489@gmail.com>
2025-05-23 20:31:21 +0700

    updated 23/05/2025 20:30

--- index.ts
+++ index.ts
@@ -37,21 +37,25 @@
 //   console.log(e);
 // };
 
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(":memory:");
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database(":memory:");
 
-db.serialize(() => {
-  db.run("CREATE TABLE lorem (info TEXT)");
+// db.serialize(() => {
+//   db.run("CREATE TABLE lorem (info TEXT)");
 
-  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-    stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
+//   const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+//   for (let i = 0; i < 10; i++) {
+//     stmt.run("Ipsum " + i);
+//   }
+//   stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM lorem", (_err: any, row: any) => {
-    console.log(row.id + ": " + row.info);
-  });
-});
+//   db.each("SELECT rowid AS id, info FROM lorem", (_err: any, row: any) => {
+//     console.log(row.id + ": " + row.info);
+//   });
+// });
 
-db.close();
+// db.close();
+
+setInterval(() => {
+    console.log("Hello ")
+}, 1000);
